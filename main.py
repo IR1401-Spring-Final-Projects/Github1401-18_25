b6b66422b420d76d2130fba45d9aba970f747f07
Mahdiyeh <mahdiyeh.ep@gmail.com>
2022-07-26 20:29:42 +0430

    package setup

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from tkinter import *
 import funcs
-# import Third.boolean.BooleanRetrival
-# import Third.preprocess.tokens_df
-
-from ..Third.boolean import BooleanRetrival
-from ..Third.preprocess import tokens_df
+from Third import  preprocess
+from Third import boolean
 
 window = Tk()
 window.title("Advanced Information Retrieval Project")
 window.geometry("1000x572")
 
 #Background
-bg = PhotoImage(file="assets/background.png")
+bg = PhotoImage(file="UI/assets/background.png")
 canvas1 = Canvas(window, width=1000, height=572)
 canvas1.pack(fill="both", expand=True)
 canvas1.create_image(0, 0, image=bg, anchor="nw")
@@ -19,12 +16,13 @@ canvas1.create_image(0, 0, image=bg, anchor="nw")
 #Search Box
 input_label = Label(text = "Enter an input:").place(x = 350, y = 120)
 
+input = Text(window, height = 8, width = 60, bg = "gray").place(x=350, y=150)
+output = Text(window, height=8, width=60, bg="white").place(x=350, y=400)
+
 def Take_input():
     inp = input.get("1.0", "end-1c")
+    return inp
 
-input = Text(window, height = 8, width = 60, bg = "gray").place(x=350, y=150)
-
-output = Text(window, height=8, width=60, bg="white").place(x=350, y=400)
 
 #Buttons
 
@@ -47,14 +45,15 @@ boolean_btn = Button(window, text = "Boolean", command=funcs.onclick_boolean()).
 #Query Expansion
 
 expansion_var = IntVar()
-model = BooleanRetrival(tokens_df)
+model = boolean.BooleanRetrival(preprocess.tokens_df)
 
 def is_checked():
+    string = Take_input()
     if expansion_var.get() == 1:
-        result = model.expand_query('Hardware cores OR Arduino AND API')
+        result = model.expand_query(string)
         output.insert(END, result)
     else:
-        result = model.process_query('Hardware cores OR Arduino AND API')
+        result = model.process_query(string)
         output.insert(END, result)
 expansion_btn = Checkbutton(window, text="Query Expansion", variable=expansion_var, onvalue=1, offvalue=0, height=1, width=15, command=is_checked()).place(x=350, y=300)
 
