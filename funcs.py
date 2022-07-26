46d72f73153fbb45cfadbf0651a0de62744829ef
Mahdiyeh <mahdiyeh.ep@gmail.com>
2022-07-26 20:57:10 +0430

    update UI

--- funcs.py
+++ funcs.py
@@ -1,23 +1,35 @@
 def onclick_boolean():
-    print("sth")
+    if expansion_var.get() == 1:
+        result = model.expand_query(string)
+        output.insert(END, result)
+    else:
+        result = model.process_query(string)
+        output.insert(END, result)
+
 
 def onclick_tfidf():
     pass
 
+
 def onclick_fasttext():
     pass
+
 
 def onclick_transformer():
     pass
 
+
 def onclick_elastic():
     pass
+
 
 def onclick_pagerank():
     pass
 
+
 def onclick_classification():
     pass
+
 
 def onclick_clustering():
     pass
