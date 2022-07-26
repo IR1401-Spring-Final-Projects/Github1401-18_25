f305b0fef59d2f262fa20cf2d9565f55806241df
Mahdiyeh <mahdiyeh.ep@gmail.com>
2022-07-26 15:15:41 +0430

    update UI

---
+++ UI/funcs.py
@@ -0,0 +1,32 @@
+import main
+
+def onclick_boolean():
+    print("sth")
+
+def onclick_tfidf():
+    pass
+
+def onclick_fasttext():
+    pass
+
+def onclick_transformer():
+    pass
+
+def onclick_elastic():
+    pass
+
+def onclick_pagerank():
+    pass
+
+def onclick_classification():
+    pass
+
+def onclick_clustering():
+    pass
+
+def is_checked():
+    if main.expansion_var.get() == 1:
+        #using query expansion
+        pass
+    else:
+        pass
