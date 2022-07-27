0b6561f7e30844212fd146d6fbc79bb8fafde592
Mahdiyeh <mahdiyeh.ep@gmail.com>
2022-07-27 15:04:44 +0430

    boolean edition

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ canvas1.pack(fill="both", expand=True)
 canvas1.create_image(0, 0, image=bg, anchor="nw")
 
 #Search Box
-input_label = Label(text = "Enter an input:").place(x = 350, y = 120)
-input = Text(window, height = 8, width = 60, bg = "gray")
-input.place(x=350, y=150)
+input_label = Label(text = "Enter an input:").place(x = 350, y = 50)
+input = Text(window, height = 5, width = 60, bg = "gray")
+input.place(x=350, y=70)
 output = Text(window, height=8, width=60, bg="white")
-output.place(x=350, y=400)
+output.place(x=350, y=310)
 
 
 def get_input():
@@ -33,14 +33,22 @@ def get_input():
     return string
 
 expansion_var = IntVar()
-expansion_btn = Checkbutton(window, text="Query Expansion", variable=expansion_var, onvalue=1, offvalue=0, height=1, width=15).place(x=350, y=300)
-model = boolean.BooleanRetrival(preprocess.tokens_df)
+expansion_btn = Checkbutton(window, text="Query Expansion", variable=expansion_var, onvalue=1, offvalue=0, height=1, width=15).place(x=350, y=170)
+
+#Models-----------------------------------------------------
+
+bool_model = boolean.BooleanRetrival(preprocess.tokens_df)
+# tfidf_model = model = Tfidf()
+# ft_model = FastTextModel(tokens_df, train=False, k=10)
+
+
+#Boolean----------------------------------------------
 
 def onclick_boolean():
     if expansion_var.get() == 1:
-        result = model.expand_query(get_input())
+        result = bool_model.expand_query(get_input())
     else:
-        result = model.process_query(get_input())
+        result = bool_model.process_query(get_input())
 
         result = [x['url'] for x in result]
     output.delete("1.0", "end")
@@ -52,26 +60,31 @@ def onclick_boolean():
                 result[i], hyperlink.add(partial(webbrowser.open, result[i])))
         output.insert(END, '\n')
 
+boolean_btn = Button(window, text = "Boolean", command=onclick_boolean).place(x=350, y=210)
 
-boolean_btn = Button(window, text = "Boolean", command=onclick_boolean).place(x=350, y=350)
-
+#Tf-Idf---------------------------------------------
+#
 # def onclick_tfidf():
 #     if expansion_var.get() == 1:
-#         result = model.print_results(query = string, expand = True)
+#         result = tfidf_model.print_results(query = get_input(), expand = True)
 #         output.insert(END, result)
 #     else:
-#         result = model.print_results(query = string)
+#         result = tfidf_model.print_results(query = get_input())
 #         output.insert(END, result)
-
-
+#
+# tfidf_btn = Button(window, text = "Tf-Idf", command=onclick_tfidf).place(x=410, y=210)
+#
+# #Fasttext---------------------------------------------------------
+#
 # def onclick_fasttext():
 #     if expansion_var.get() == 1:
-#         result = model.print_results(query=string, expand=True)
+#         result = ft_model.print_results(query=get_input(), expand=True)
 #         output.insert(END, result)
 #     else:
-#         result = model.print_results(query=string)
+#         result = ft_model.print_results(query=get_input())
 #         output.insert(END, result)
-
+#
+# fasttext_btn = Button(window, text = "Fasttext", command=onclick_fasttext).place(x=455, y=210)
 
 def onclick_transformer():
     pass
@@ -92,18 +105,17 @@ def onclick_classification():
 def onclick_clustering():
     pass
 
-# tfidf_btn = Button(window, text = "Tf-Idf", command=funcs.onclick_tfidf()).place(x=410, y=350)
+
 #
-# fasttext_btn = Button(window, text = "Fasttext", command=funcs.onclick_fasttext()).place(x=455, y=350)
 #
-# transformer_btn = Button(window, text = "Transformer", command=funcs.onclick_transformer()).place(x=510, y=350)
-#
-# elastic_btn = Button(window, text = "Elastic search", command=funcs.onclick_elastic()).place(x=590, y=350)
-#
-# pagerank_btn = Button(window, text = "Page Rank", command=funcs.onclick_pagerank()).place(x=675, y=350)
-#
-# classification_btn = Button(window, text = "Classification", command=funcs.onclick_classification()).place(x=350, y=400)
-#
-# clustering_btn = Button(window, text = "Clustering", command=funcs.onclick_clustering()).place(x=435, y=400)
+transformer_btn = Button(window, text = "Transformer", command=onclick_transformer).place(x=510, y=210)
+
+elastic_btn = Button(window, text = "Elastic search", command=onclick_elastic).place(x=590, y=210)
+
+pagerank_btn = Button(window, text = "Page Rank", command=onclick_pagerank).place(x=675, y=210)
+
+classification_btn = Button(window, text = "Classification", command=onclick_classification).place(x=350, y=250)
+
+clustering_btn = Button(window, text = "Clustering", command=onclick_clustering).place(x=435, y=250)
 
 mainloop()
